e04eecd63a88f976ec4fccfd1b6bc950c9dac7d5
Roi Klevansky <roiklevansky@gmail.com>
2022-06-28 13:26:50 +0300

    Added docstrings to all functions, and added raw strings to regex patterns.

--- src/index.py
+++ src/index.py
@@ -7,7 +7,44 @@ import xlsxwriter
 
 
 class Lead:
+    """
+    Class representing a single lead(a form that is received automatically and contains contact information of a
+    potential client)
+
+    Attributes
+    ----------
+    phone : str
+        The phone number of the client
+    email : str
+        The email of the client
+    name : str
+        The name of the client
+    destination : str
+        The trip destination
+    people_num : str
+        The number of people in the trip
+    kosher : str
+        The kosher status of the trip
+    details : str
+        The added details of the lead
+
+    Methods
+    -------
+    to_list()
+        Returns a list of the attributes of the lead
+    """
+
     def __init__(self, phone='', email='', name='', destination='', people_num='', kosher='', details=''):
+        """
+        Parameters are the attributes of the lead
+        :param phone: str - The phone number of the client
+        :param email: str - The email of the client
+        :param name: str - The name of the client
+        :param destination: str - The trip destination
+        :param people_num: str - The number of people in the trip
+        :param kosher: str - The kosher status of the trip
+        :param details: str - The added details of the lead
+        """
         self.phone = phone
         self.email = email
         self.name = name
@@ -17,16 +54,25 @@ class Lead:
         self.details = details
 
     def to_list(self):
+        """
+        Returns a list of the attributes of the lead
+        :return: list - The attributes of the lead
+        """
         return [self.phone, self.email, self.name, self.destination, self.people_num, self.kosher, self.details]
 
 
 def parse_telecall_lead(lead: str) -> Lead:
-    phone_regex = 'טלפון לחזרה\s*(\d+)'
-    name_regex = 'שם מלא:\s*([ \'א-ת]+)'
-    destination_regex = 'יעד נסיעה:\s*([ \'א-ת]+)'
-    people_num_regex = 'מספר נפשות:\s*([ \dא-ת]+)'
-    kosher_regex = 'סוג טיול:\s*([ א-ת]+)'
-    details_regex = 'ההודעה:\s*([^\n\r]+)'
+    """
+    Parses a lead from a telecall lead string
+    :param lead: str - The lead string
+    :return: Lead - The parsed lead
+    """
+    phone_regex = r'טלפון לחזרה\s*(\d+)'
+    name_regex = r'שם מלא:\s*([ \'א-ת]+)'
+    destination_regex = r'יעד נסיעה:\s*([ \'א-ת]+)'
+    people_num_regex = r'מספר נפשות:\s*([ \dא-ת]+)'
+    kosher_regex = r'סוג טיול:\s*([ א-ת]+)'
+    details_regex = r'ההודעה:\s*([^\n\r]+)'
 
     lead_obj = Lead()
     phone_match = re.search(phone_regex, lead)
@@ -51,9 +97,14 @@ def parse_telecall_lead(lead: str) -> Lead:
 
 
 def parse_telecall_interested(lead: str) -> Lead:
-    phone_regex = 'טלפון לחזרה\s*(\d+)'
-    name_regex = 'שם הפונה\s:\s*([ \'א-ת]+)'
-    details_regex = 'ההודעה:\s*([^\n\r]+)'
+    """
+    Parses a lead from a telecall interested message string
+    :param lead: str - The lead string
+    :return: Lead - The parsed lead
+    """
+    phone_regex = r'טלפון לחזרה\s*(\d+)'
+    name_regex = r'שם הפונה\s:\s*([ \'א-ת]+)'
+    details_regex = r'ההודעה:\s*([^\n\r]+)'
 
     lead_obj = Lead()
     phone_match = re.search(phone_regex, lead)
@@ -69,9 +120,14 @@ def parse_telecall_interested(lead: str) -> Lead:
 
 
 def parse_telecall_general_message(lead: str) -> Lead:
-    phone_regex = 'טלפון לחזרה\s*(\d+)'
-    name_regex = 'שם מלא:\s*([ \'א-ת]+)'
-    details_regex = 'ההודעה:\s*([^\n\r]+)'
+    """
+    Parses a lead from a telecall general message string
+    :param lead: str - The lead string
+    :return: Lead - The parsed lead
+    """
+    phone_regex = r'טלפון לחזרה\s*(\d+)'
+    name_regex = r'שם מלא:\s*([ \'א-ת]+)'
+    details_regex = r'ההודעה:\s*([^\n\r]+)'
 
     lead_obj = Lead()
     phone_match = re.search(phone_regex, lead)
@@ -87,10 +143,15 @@ def parse_telecall_general_message(lead: str) -> Lead:
 
 
 def parse_telecall_customer_support(lead: str) -> Lead:
-    phone_regex = 'טלפון לחזרה\s*(\d+)'
-    name_regex = 'שם מלא:\s*([ \'א-ת]+)'
-    destination_regex = 'יעד נסיעה:\s*([ \'א-ת]+)'
-    details_regex = 'ההודעה:\s*([^\n\r]+)'
+    """
+    Parses a lead from a telecall customer support string
+    :param lead: str - The lead string
+    :return: Lead - The parsed lead
+    """
+    phone_regex = r'טלפון לחזרה\s*(\d+)'
+    name_regex = r'שם מלא:\s*([ \'א-ת]+)'
+    destination_regex = r'יעד נסיעה:\s*([ \'א-ת]+)'
+    details_regex = r'ההודעה:\s*([^\n\r]+)'
 
     lead_obj = Lead()
     phone_match = re.search(phone_regex, lead)
@@ -109,11 +170,16 @@ def parse_telecall_customer_support(lead: str) -> Lead:
 
 
 def parse_virtual_chat_lead(lead: str) -> Lead:
+    """
+    Parses a lead from a virtual chat log string
+    :param lead: str - The lead string
+    :return: Lead - The parsed lead
+    """
     # TODO: This format saves some data on the client embedded to the message itself,
     # for now this data is unused. Fix this someday.
-    phone_regex = 'מספר טלפון:\s*(\d+)'
-    email_regex = 'כתובת מייל:\s*(\w+@\w+.[.\w]+)'
-    name_regex = 'משתמש\s([ \'א-תa-zA-Z]+)'
+    phone_regex = r'מספר טלפון:\s*(\d+)'
+    email_regex = r'כתובת מייל:\s*(\w+@\w+.[.\w]+)'
+    name_regex = r'משתמש\s([ \'א-תa-zA-Z]+)'
 
     lead_obj = Lead()
     phone_match = re.search(phone_regex, lead)
@@ -131,10 +197,15 @@ def parse_virtual_chat_lead(lead: str) -> Lead:
 
 
 def parse_trip_page_lead(lead: str) -> Lead:
-    phone_regex = 'טלפון:\s*(\d+)'
-    name_regex = 'שם(?: מלא)?:\s*([ \'א-ת]+)'
-    email_regex = 'דואר (?:האלקטרוני|אלקטרוני):\s*(\w+@\w+.[.\w]+)'
-    details_regex = '(?:הודעה|נושא):\s*([^\n\r]+)'
+    """
+    Parses a lead from a trip page(Contact Us) string
+    :param lead: str - The lead string
+    :return: Lead - The parsed lead
+    """
+    phone_regex = r'טלפון:\s*(\d+)'
+    name_regex = r'שם(?: מלא)?:\s*([ \'א-ת]+)'
+    email_regex = r'דואר (?:האלקטרוני|אלקטרוני):\s*(\w+@\w+.[.\w]+)'
+    details_regex = r'(?:הודעה|נושא):\s*([^\n\r]+)'
 
     lead_obj = Lead()
     phone_match = re.search(phone_regex, lead)
@@ -153,10 +224,15 @@ def parse_trip_page_lead(lead: str) -> Lead:
 
 
 def parse_contact_us_form(lead: str) -> Lead:
-    phone_regex = 'טלפון:\s*(\d+)'
-    name_regex = 'שם(?: מלא)?:\s*([ \'א-ת]+)'
-    email_regex = 'דואר (?:האלקטרוני|אלקטרוני):\s*(\w+@\w+.[.\w]+)'
-    details_regex = '(?:הודעה|נושא):\s*([^\n\r]+)'
+    """
+    Parses a lead from a contact us form string
+    :param lead: str - The lead string
+    :return: Lead - The parsed lead
+    """
+    phone_regex = r'טלפון:\s*(\d+)'
+    name_regex = r'שם(?: מלא)?:\s*([ \'א-ת]+)'
+    email_regex = r'דואר (?:האלקטרוני|אלקטרוני):\s*(\w+@\w+.[.\w]+)'
+    details_regex = r'(?:הודעה|נושא):\s*([^\n\r]+)'
 
     lead_obj = Lead()
     phone_match = re.search(phone_regex, lead)
@@ -175,16 +251,21 @@ def parse_contact_us_form(lead: str) -> Lead:
 
 
 def parse_lead(lead: str) -> Lead | None:
+    """
+    Parses a lead from a string, returns None if the lead is not parsable or should be ignored
+    :param lead: str - The lead string
+    :return: Lead - The parsed lead
+    """
     title = lead[0]
-    telecall_interested_notice_regex = '^טלקול,הודעות עבור תיירות-'
-    telecall_lead_regex = '^טלקול,לקוחות חדשים-'
-    telecall_general_message_regex = '^טלקול,הודעות כלליות-'
-    telecall_customer_support_regex = '^טלקול,שירות לקוחות-'
-    telecall_grouped_regex = 'טלקול, ריכוז הודעות'
-    virtual_chat_lead_regex = '^ליד חדש מהאתר'
-    trip_page_lead_regex = '^דף טיול'
-    contact_us_regex = 'טופס יצירת קשר'
-    disarmed_regex = '(מכירה|רכישה)\s\d+\s(בוצעה|בוצע)\sבהצלחה'
+    telecall_interested_notice_regex = r'^טלקול,הודעות עבור תיירות-'
+    telecall_lead_regex = r'^טלקול,לקוחות חדשים-'
+    telecall_general_message_regex = r'^טלקול,הודעות כלליות-'
+    telecall_customer_support_regex = r'^טלקול,שירות לקוחות-'
+    telecall_grouped_regex = r'טלקול, ריכוז הודעות'
+    virtual_chat_lead_regex = r'^ליד חדש מהאתר'
+    trip_page_lead_regex = r'^דף טיול'
+    contact_us_regex = r'טופס יצירת קשר'
+    disarmed_regex = r'(מכירה|רכישה)\s\d+\s(בוצעה|בוצע)\sבהצלחה'
 
     if re.search(telecall_interested_notice_regex, title):
         return parse_telecall_interested(lead[1])
@@ -211,6 +292,11 @@ def parse_lead(lead: str) -> Lead | None:
 
 
 def parse_leads_file(file_name: str) -> list:
+    """
+    Parses a file of leads, returns a list of raw leads
+    :param file_name: str - The file name
+    :return: list - The list of leads in format (lead_title, lead_body)
+    """
     df = pd.read_csv(file_name, encoding='windows-1255')
     leads = []
     titles_list = df['נושא'].tolist()
@@ -221,6 +307,12 @@ def parse_leads_file(file_name: str) -> list:
 
 
 def export_leads_to_xlsx(leads: list, file_name: str):
+    """
+    Exports a list of parsed leads to a xlsx file
+    :param leads: list - The list of parsed leads
+    :param file_name: str - The file name
+    :return: None
+    """
     workbook = xlsxwriter.Workbook(file_name)
     worksheet = workbook.add_worksheet()
     worksheet.right_to_left()
@@ -249,11 +341,15 @@ def export_leads_to_xlsx(leads: list, file_name: str):
 
 
 def main():
+    """
+    Main function
+    :return: None
+    """
     raw_leads = parse_leads_file(askopenfilename())
     parsed_leads = []
     for lead in raw_leads:
         parsed_lead = parse_lead(lead)
-        if parsed_lead != None:
+        if parsed_lead is not None:
             parsed_leads.append(parsed_lead)
     files = [('All Files', '*.*'), ('Excel File', '*.xlsx')]
     file = asksaveasfilename(filetypes=files, defaultextension=".xlsx")
